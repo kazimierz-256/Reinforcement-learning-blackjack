755553753f10f5d239871a65471d353d0ea5c301
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2019-11-28 14:12:59 +0100

    refactored tests

--- game_logic_test.py
+++ game_logic_test.py
@@ -3,18 +3,20 @@ import game_logic
 import pytest
 
 
-@pytest.mark.parametrize("cards,reference_values", [
-    ([Card.JACK, Card.ACE], [11, 21]),
-    ([Card.JACK, Card.ACE, Card.ACE], [12, 22, 32]),
+@pytest.mark.parametrize("cards, all_values, nonbusting_values", [
+    ([Card.JACK, Card.ACE], [11, 21], [11, 21]),
+    ([Card.ACE, Card.ACE],  [2, 12, 22], [2, 12]),
+    ([Card.ACE, Card.ACE, Card.ACE, Card.ACE],  [4, 14, 24, 34, 44], [4, 14]),
+    ([Card.QUEEN, Card.KING, Card.TWO],  [22], []),
+    ([Card.JACK, Card.QUEEN, Card.ACE], [21, 31], [21]),
+    ([Card.JACK, Card.ACE, Card.ACE], [12, 22, 32], [12]),
+    ([], [0], [0]),
+    ([Card.SEVEN, Card.SEVEN, Card.SEVEN], [21], [21]),
+    ([Card.SEVEN, Card.SEVEN, Card.EIGHT], [22], []),
+    ([Card.TWO], [2], [2]),
 ])
-def test_evaluate_deck_values(cards, reference_values):
-    values = list(sorted(game_logic.evaluate_deck_values(cards)))
-    assert reference_values == values
-
-
-@pytest.mark.parametrize("cards,reference_values", [
-    ([Card.JACK, Card.ACE, Card.ACE], [12]),
-])
-def test_evaluate_deck_values_nonbusting(cards, reference_values):
-    values = list(sorted(game_logic.evaluate_nonbusting_deck_values(cards)))
-    assert reference_values == values
+def test_evaluate_deck_values_busting_and_nonbusting(cards, all_values, nonbusting_values):
+    busting_values = list(sorted(game_logic.evaluate_deck_values(cards)))
+    assert all_values == busting_values
+    nonbusting_values = list(sorted(game_logic.evaluate_nonbusting_deck_values(cards)))
+    assert nonbusting_values == nonbusting_values
