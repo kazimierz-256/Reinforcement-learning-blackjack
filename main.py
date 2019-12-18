c183ba4468898918a1d0a9ea8ed7f225ce7bde0d
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2019-12-18 13:32:25 +0100

    developing environment from fixed strategy and learning from it

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ import player
 import dealer
 import visualization
 import random_learning_strategy
+import random_fixed_strategy
 import environment_model
 
 
 if __name__ == "__main__":
     # reinforcement learning parameters
-    episode_count_list = [200_000]
-    print_status_every_n_episodes = 10_000
+    episode_count_list = [10_000]
+    print_status_every_n_episodes = 1_000
     # also known as gamma
     discount_factor = 1.0
     random_seed = 123
@@ -31,12 +32,17 @@ if __name__ == "__main__":
 
             # initial setup
             player_environment_model = environment_model.EnvironmentModel()
-            player_strategy = random_learning_strategy.Random_learning_strategy(
+            player_fixed_strategy = random_fixed_strategy.Random_fixed_strategy(
+                environment_model=player_environment_model,
+                default_probability_of_stand=default_probability_of_stand
+                )
+            player_learning_strategy = random_learning_strategy.Random_learning_strategy(
                 environment_model=player_environment_model,
                 probability_of_random_choice=probability_of_random_choice,
                 default_probability_of_stand=default_probability_of_stand
                 )
-            player_instance = player.Player(strategy=player_strategy)
+                
+            player_instance = player.Player(strategy=player_fixed_strategy)
             dealer_win_count = 0
             player_win_count = 0
             player_win_history = []
@@ -45,7 +51,7 @@ if __name__ == "__main__":
             # total_player_win_average = 0
             draw_count = 0
 
-            # playing blackjack games
+            # playing blackjack games with fixed strategy
             for episode_no in range(episode_count):
                 # 1/(1 + episode_no) #probability_of_random_choice
                 player_instance.probability_of_random_choice = probability_of_random_choice
@@ -95,5 +101,61 @@ if __name__ == "__main__":
                 if (episode_no+1) % print_status_every_n_episodes == 0:
                     print("\n".join(messages))
                     visualization.draw_strategy_heatmap(player_environment_model)
+
+            print("Learning from fixed strategy")
+
+            # playing blackjack games and learning
+            for episode_no in range(episode_count):
+                # 1/(1 + episode_no) #probability_of_random_choice
+                player_instance.probability_of_random_choice = probability_of_random_choice
+                game_status, player_visited_bare_states = game_logic.play(player_instance)
+                player_final_reward = game_logic.get_player_reward(game_status)
+                player_instance.end_game(
+                    player_final_reward, player_visited_bare_states, discount_factor)
+
+                # game ended, check who has won the game
+                if game_status == game_definitions.Status.DEALER_WON:
+                    dealer_win_count += 1
+                    player_win_history.append(0)
+
+                    last_element = player_win_history_average[-1]
+                    n = len(player_win_history_average)
+                    player_win_history_average.append(
+                        last_element + (0-last_element)/n)
+                    # total_player_win_average += (0-total_player_win_average)/n
+                elif game_status == game_definitions.Status.PLAYER_WON:
+                    player_win_count += 1
+                    player_win_history.append(1)
+
+                    last_element = player_win_history_average[-1]
+                    n = len(player_win_history_average)
+                    player_win_history_average.append(
+                        last_element + (1-last_element)/n)
+                    # total_player_win_average += (1-total_player_win_average)/n
+                elif game_status == game_definitions.Status.DRAW:
+                    draw_count += 1
+                else:
+                    raise Exception(f"Unexpected game state: {game_status}")
+
+                latest_history = player_win_history
+                # latest_history = player_win_history_average[-latest_entry_count_for_summary:]
+                latest_ratio_average = np.average(latest_history)
+                latest_ratio_std = np.std(latest_history)
+                player_win_history_std.append(latest_ratio_std)
+
+                # printing all lines at once in order to avoid console text flickering
+                messages = [
+                    f"Latest {latest_entry_count_for_summary} games summary: average player wins/dealer wins: {latest_ratio_average: .5f} standard deviation: {latest_ratio_std: .3E}",
+                    f"Completed episodes {episode_no + 1} out of {episode_count}",
+                    str(len(player_environment_model.state_to_value))
+                    # f"Total statistics: average player wins/dealer wins: {total_player_win_average: .5f}"
+                ]
+
+                if (episode_no+1) % print_status_every_n_episodes == 0:
+                    print("\n".join(messages))
+                    visualization.draw_strategy_heatmap(player_environment_model)
+
+
+
             if show_plots_in_browser_tab:
                 visualization.draw_figure(player_win_history, player_win_history_average, player_win_history_std)
